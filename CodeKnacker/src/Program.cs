9ad4df3eee12a808fa007fd662d2370dd4064771
TheGreatMisconception <114302667+TheGreatMisconception@users.noreply.github.com>
2022-09-24 18:24:54 +0200

    Create Variant_1

---
+++ CodeKnacker/src/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CodeKnacker
+{
+    class MainClass
+    {
+        private static bool CanConvert(string num)
+        {
+            int CNum;
+            try { 
+                CNum = Convert.ToInt32(num);
+            }
+            catch {
+                return false;
+            }
+            return true;
+        }
+
+        private static bool GuessTheNumber()
+        {
+            int Number;
+            int INumber;
+            bool Guessed;
+            string Guess;
+            Guessed = false;
+            Number = 15;
+            while (Guessed != true) {
+                Console.WriteLine("Enter number: ");
+                Guess = Console.ReadLine();
+                if (CanConvert(Guess))
+                {
+                    INumber = Convert.ToInt32(Guess);
+                    if (Number == INumber) {
+                        Console.WriteLine("Congratulations, you guessed the right number\n\nPress any key to exit...");
+                        Console.ReadKey();
+                        Guessed = true;
+                    }
+                    else {
+                        Console.WriteLine("{0} is not the number you're looking for, try again\n", INumber);
+                    
+                    }
+                }
+
+
+            }
+            return false;
+        }
+
+        public static void Main(string[] args)
+        {
+            string Version;
+            Version = "0.1 Variant_1";
+            Console.WriteLine("Welcome to CodeKnacker!\nVersion: {0}\n\n", Version);
+            GuessTheNumber();
+        }
+    }
+}
